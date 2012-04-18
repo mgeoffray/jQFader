d88245b6d424de60a5dae726cacba19425468930
unknown <mattheo.geoffray+github@gmail.com>
2012-04-18 22:50:34 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
+// Examples
 jQuery(document).ready(function(){
 	init();
 });
-function restart(){
-	jQuery('.no-display,.fader,.random,.reverse').css({'display':'none'});
-	init();
-}
 function init(){
 	jQuery('.fader').jqfader({callback:showRestart});// Simple effect with callback
 }
 function showRestart(){
 	jQuery('.restart').fadeTo(300,1);
+}
+function restart(){
+	jQuery('.restart,.fader').css({'display':'none'});
+	init();
 }
