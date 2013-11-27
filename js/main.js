fa11e0c1bfcd738e6652eb40b350e679c75e7eda
unknown <mattheo.geoffray+github@gmail.com>
2013-11-27 21:34:13 +0100

    Add comments to JS (main.js)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
-// Examples
+/**************************
+ **** Main.js
+ **************/
+
 jQuery(document).ready(function(){
 	init();
 });
+
+/** Launch demo **/
 function init(){
 	jQuery('.fader').jqfader({callback:showRestart});// Simple effect with callback
 }
+/** Show restart button **/
 function showRestart(){
 	jQuery('.restart').fadeTo(300,1);
 }
+/** Restart demo **/
 function restart(){
 	jQuery('.restart,.fader').css({'display':'none'});
 	init();
